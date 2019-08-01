15e3c119b478433dcbc8ccaaec60a850d99b511b
tetedestup <45398660+tetedestup@users.noreply.github.com>
2019-08-01 15:15:02 +0200

    commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# epec-custom-palette
+#epec-custom-palette
+
+#created by Sami Betka
+
+#This is a WordPress plugin. it is used to change color palette for an article in your WordPress site.
+
+#To install it, you need to download the zip file and extract it to the plugin folder in wp-content (mywordpress\app\public\wp-content\plugins\epec-custom-palette).
+
+#If you want to change the color palette, just change the colors and their names in the php file (epec-custom-palette.php), and edit the style.css file.
--- epec-custom-palette.php
+++ epec-custom-palette.php
@@ -1,1 +1,43 @@
 <?php
+/*
+Plugin Name: epec-custom-palette
+Plugin URI: http://zero-plugin.com
+Description: Un plugin d'introduction pour le développement sous WordPress
+Version: 0.1
+Author: Sami
+Author URI: http://votre-site.com
+License: GPL2
+*/
+
+function myfunction_by_myself() {
+    add_theme_support( 'editor-color-palette', array(
+        array(
+            'name' => __( 'Brand Color 1', 'epec-custom-palette' ),
+            'slug' => 'Brand Color 1',
+            'color' => '#6128A1',
+        ),
+        array(
+            'name' => __( 'Brand Color 2', 'epec-custom-palette' ),
+            'slug' => 'Brand Color 2',
+            'color' => '#E94B59',
+        ),
+        array(
+            'name' => __( 'Very Dark Gray', 'epec-custom-palette' ),
+            'slug' => 'Very Dark Gray',
+            'color' => '#111111',
+        ),
+        array(
+            'name' => __( 'White', 'epec-custom-palette' ),
+            'slug' => 'White',
+            'color' => '#FFFFFF',
+        ),
+    ) );
+}
+ 
+add_action( 'after_setup_theme', 'myfunction_by_myself' );
+
+function my_scripts() {
+    wp_register_style( 'prefix-style', plugins_url('style.css', __FILE__) );
+    wp_enqueue_style( 'prefix-style' );
+    }
+    add_action('wp_enqueue_scripts','my_scripts');
